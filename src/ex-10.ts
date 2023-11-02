ffa01c73431a76ceaf116b279809830c1fa22495
matin889 <bornlitematin@gmail.com>
2023-11-02 10:36:22 +0100

    2 more exercises completed

--- src/ex-10.ts
+++ src/ex-10.ts
@@ -1,12 +1,30 @@
 // DIN UPPGIFT: Utför fråga 1-3 med kodexempel i Typescript / 3 poäng
 
-
 // 1. Ge ett exempel på en funktion med en generiskt typ.
 
-
+const Colors = <T>(reverseColors: T[]): T[] => {
+  return reverseColors.reverse();
+};
+const colors = ["Red", "Blue", "Green"];
+const showColors = Colors(colors);
+console.log(showColors);
 
 // 2. Ge ett exempel på en funktion med två generiska typer.
-
-
-
+function matched<T, U>(key: T, value: U): { key: T; value: U } {
+  return { key, value };
+}
+const colorsMatched = matched("name", "Green");
+const numbers = matched("number", 100);
+console.log(colorsMatched);
+console.log(numbers);
 // 3. Ge ett exempel på ett interface med en generisk typ.
+interface Shapes<T> {
+  shape: T;
+}
+
+function showShaapes<T>(shapes: Shapes<T>): T {
+  return shapes.shape;
+}
+
+const fourArms: Shapes<string> = { shape: "Squaare, Rectangle" };
+console.log(fourArms);
--- src/ex-8.ts
+++ src/ex-8.ts
@@ -1,24 +1,34 @@
-// DIN UPPGIFT: Använda enums för att skapa olika användarroller som ska vara "Admin", "Member" eller "Guest". 
-// Du ska även implementera funktionen assignBadgeColor som ska returnera en färg baserat på rollen. 
+// DIN UPPGIFT: Använda enums för att skapa olika användarroller som ska vara "Admin", "Member" eller "Guest".
+// Du ska även implementera funktionen assignBadgeColor som ska returnera en färg baserat på rollen.
 // Admin ska returnera "Red", Member ska returnera "Blue" och Guest ska returnera "Green". / 3 poäng
 
-
 // 1. Definerier enums för UserRoles Adming, Member och Guest
- enum UserRole {
-   // Här...
+enum UserRole {
+  Admin,
+  Member,
+  Guest,
+}
+
+interface User {
+  name: string;
+  role: UserRole;
+}
+
+// 2. Implementera assignBadgeColor-funktionen som ska returnera en färg baserat på användarens roll.
+function assignBadgeColor(user: User): string {
+  switch (user.role) {
+    case UserRole.Admin:
+      return "Red";
+      break;
+    case UserRole.Member:
+      return "Blue";
+      break;
+    case UserRole.Guest:
+      return "Green";
+      break;
   }
-  
-  interface User {
-    name: string;
-    role: UserRole;
-  }
-  
-  // 2. Implementera assignBadgeColor-funktionen som ska returnera en färg baserat på användarens roll.
-  function assignBadgeColor(user: User): string {
-        // Här...
-  }
-  
-  // Testa funktionen
-  const user1: User = { name: "Alice", role: UserRole.Admin };
-  console.log(assignBadgeColor(user1)); // Ska skriva ut "Red".
-  
+}
+
+// Testa funktionen
+const user1: User = { name: "Alice", role: UserRole.Admin };
+console.log(assignBadgeColor(user1)); // Ska skriva ut "Red".
