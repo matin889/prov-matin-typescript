6bc5c84ce6f9b0f6706cafd2689d6bbb9e97522d
matin889 <bornlitematin@gmail.com>
2023-11-03 10:24:49 +0100

    2 more exercises completed

--- src/ex-7.ts
+++ src/ex-7.ts
@@ -1,28 +1,43 @@
 // DIN UPPGIFT: Definiera en index signature för Dictionary-typen samt implementera funktionerna addWord och getDefinition. / 3 poäng
 
 type Dictionary = {
-    // Index signature ska vara här
- 
-  };
-  
-  
-  function addWord(dictionary: Dictionary, word: string, definition: string): void {
-    // Implementera funktionen så att den lägger till ordet och definitionen i ordboken (dictionary).
-    
-  }
-  
+  [key: string]: string;
+};
 
-  function getDefinition(dictionary: Dictionary, word: string): string | undefined {
-    // Implementera funktionen så att den returnerar definitionen av ordet (word) om det finns i ordboken (dictionary).
-    // Om ordet inte finns ska funktionen returnera undefined.
-  }
+function addWord(
+  dictionary: Dictionary,
+  word: string,
+  definition: string
+): void {
+  // Implementera funktionen så att den lägger till ordet och definitionen i ordboken (dictionary).
 
-  
-  // Testa funktionerna
-  const myDictionary: Dictionary = {};
-  addWord(myDictionary, "TypeScript", "A superset of JavaScript that adds static typing.");
-  console.log(getDefinition(myDictionary, "TypeScript")); // Ska skriva ut "A superset of JavaScript that adds static typing."
-  addWord(myDictionary, "JavaScript", "A dynamic programming language.");
-  console.log(getDefinition(myDictionary, "JavaScript")); // Ska skriva ut "A dynamic programming language."
-  console.log(getDefinition(myDictionary, "Java")); // Ska skriva ut undefined
-  
+  dictionary[word] = definition;
+}
+
+function getDefinition(
+  dictionary: Dictionary,
+  word: string
+): string | undefined {
+  // Implementera funktionen så att den returnerar definitionen av ordet (word) om det finns i ordboken (dictionary).
+  // Om ordet inte finns ska funktionen returnera undefined.
+  if (word in dictionary) {
+    return dictionary[word];
+  } else {
+    return undefined;
+  }
+}
+
+// Testa funktionerna
+const myDictionary: Dictionary = {};
+addWord(
+  myDictionary,
+  "TypeScript",
+  "A superset of JavaScript that adds static typing."
+);
+console.log(myDictionary);
+console.log(getDefinition(myDictionary, "TypeScript")); // Ska skriva ut "A superset of JavaScript that adds static typing."
+addWord(myDictionary, "JavaScript", "A dynamic programming language.");
+console.log(getDefinition(myDictionary, "JavaScript")); // Ska skriva ut "A dynamic programming language."
+console.log(myDictionary);
+console.log(getDefinition(myDictionary, "Java")); // Ska skriva ut undefined
+console.log(myDictionary);
